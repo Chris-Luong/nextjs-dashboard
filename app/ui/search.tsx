f81c797f70ace3662b41ea156c75342c2d37a17d
Chris-Luong <64023748+Chris-Luong@users.noreply.github.com>
2024-01-18 00:02:50 +1100

    added pagination to search, chap 12 done

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -16,6 +16,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     // after every character
 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
