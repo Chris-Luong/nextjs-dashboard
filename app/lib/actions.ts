9127b1eb291ad3be1860be5eb5133014716123a7
Chris-Luong <64023748+Chris-Luong@users.noreply.github.com>
2024-01-20 23:12:46 +1100

    added authentication functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -10,6 +10,9 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -104,5 +107,24 @@ export async function deleteInvoice(id: string) {
     return { message: 'Invoice Deleted' };
   } catch (error) {
     return { message: `Database Error: Failed to Delete Invoice. ${error}` };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 
+// user@nextmail.com, 123456
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
